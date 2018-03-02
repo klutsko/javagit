df9df3f068d63b5f7b236cd1ac78b2cfe1c60862
Kseniia <36239471+klutsko@users.noreply.github.com>
2018-03-02 19:59:17 +0200

    exceptions

---
+++ hw9/Exceptions.java
@@ -0,0 +1,95 @@
+package hw9;
+
+import javax.jws.Oneway;
+
+public class Exceptions {
+    public static void main(String[] args) {
+
+        //ArrayIndexOutOfBoundsException= RuntimeException = we don't need to use try/catch here
+
+        String[] myFirstStringArray = new String[]{"String 1", "String 2",
+                "String 3"};
+
+
+        for (int i = 0; i < myFirstStringArray.length; i++) {
+            if (i >= myFirstStringArray.length) {
+                System.out.println("Index is bigger than array length");
+                myFirstStringArray[i] = null;
+            } else {
+                System.out.println("Array is " + myFirstStringArray[i]);
+
+            }
+        }
+
+        //NullPointerException = RuntimeException = we don't need to use try/catch here
+
+        Integer num=null;
+
+        if(num!=null){
+            System.out.println(num);
+
+        }else {
+            System.out.println("Num is null");
+        }
+
+
+        //ClassCastException = RuntimeException = we don't need to use try/catch here =>
+
+        int a =44;
+        long b =a;
+
+        if(a==b){
+            System.out.println(a);
+        }else{
+            System.out.println("You cannot cast this way");
+        }
+
+
+
+        //NumberFormatException = RuntimeException = we don't need to use try/catch here
+
+
+        String s = "HJH";
+        Object o = s;
+
+        if (s.equals(o)) {
+            System.out.println("Upcasting was done. Format is correct");
+
+        }else{
+            System.out.println(s);
+        }
+        
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
