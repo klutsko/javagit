ae8ec981f7f26699b97598086de07a9370ee02f3
Kseniia <36239471+klutsko@users.noreply.github.com>
2018-03-10 18:03:48 +0200

    Add files via upload

---
+++ module_1/task1/StudyBuilding.java
@@ -0,0 +1,26 @@
+package module.task1.task1;
+
+public class StudyBuilding {
+    public static void main(String[] args) {
+        int markForPassing =4;
+        Student student = new Student("Tom", "Bom");
+        Teacher teacher = new Teacher("John");
+
+        student.registration(new Faculty("Historical"));
+        student.passingExams(new Exams("History"));
+        student.passingExams(new Exams("Math"));
+        teacher.putMark(student, new Mark("Three") );
+        teacher.putMark(student, new Mark("Four"));
+
+
+
+
+
+
+    }
+    static Mark avg (Student s, Mark mark){
+        int sum =0;
+        
+        return
+    }
+}
