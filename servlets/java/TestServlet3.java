c626220bb4ef729a2a18764ccb1fbb96cebf6494
Kseniia <36239471+klutsko@users.noreply.github.com>
2018-04-25 21:56:06 +0300

    Update TestServlet3.java

--- servlets/java/TestServlet3.java
+++ servlets/java/TestServlet3.java
@@ -1,15 +1,15 @@
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.*;
 
 
 @WebServlet("/TestServlet3")
 public class TestServlet3 extends HttpServlet {
+    public static final String UserId = "UserID" ;
+
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
 
@@ -18,44 +18,44 @@ public class TestServlet3 extends HttpServlet {
          String dbPass = "161617Kk";
 
 
+/*
+
         Cookie[] cookies = httpServletRequest.getCookies();
 
-        int userID = 1;
-
-        String query = "select FirstName, UserEmail from users where id like ?;";
-
+        String query = "select FirstName, UserEmail from users where UserID=7;";
+        String firstName = "";
 
         for (Cookie c : cookies) {
-            if (c.getName().equals(TestServlet2.id)) {
-                userID = Integer.valueOf(c.getValue());
+            if (c.getName().equals(UserId)) {
                 try {
                     DriverManager.registerDriver(new com.mysql.jdbc.Driver());
                     Connection conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
                     PreparedStatement statement = conn.prepareStatement(query);
+
                     ResultSet rs= statement.executeQuery(query);
-
-                   /* String firstName = httpServletRequest.getParameter("firstname");
-                    String email = httpServletRequest.getParameter("email");*/
-
                     while (rs.next()) {
 
-                        String firstName = rs.getString(2);
-                        String email = rs.getString(3);
+          //             firstName = rs.getString(2);
+//                        String email = rs.getString(3);
+
                     }
 
+*/
+                    httpServletRequest.setAttribute("name", "TEST");
+                    httpServletRequest.getRequestDispatcher("/test3.jsp").forward(httpServletRequest, httpServletResponse);
 
-
-
+/*
                 } catch (SQLException e) {
                     e.printStackTrace();
 
 
                 }
             }else{
+
                 httpServletResponse.sendRedirect("webapp/test2.jsp");
             }
-
+*/
 
         }
-    }}
+    }
 
